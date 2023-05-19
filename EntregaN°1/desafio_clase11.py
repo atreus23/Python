5052aa05a80d89b6d7f18d3e7cb71ab6230973ad
atreus23 <tecchio2013@gmail.com>
2023-05-18 21:04:54 -0300

    Primera entrega

---
+++ EntregaN°1/desafio_clase11.py
@@ -0,0 +1,73 @@
+import json
+import os
+
+def registro():
+    Nombre = input('Ingrese su nombre: ')
+    Contrasenia = input('Ingrese su contraseña: ')
+    if os.path.exists('datos.json'):
+        Lec = open('datos.json', 'r')
+        BaseDatos = dict(json.load(Lec))
+        Lec.close()
+        lista = list(BaseDatos.keys())
+        if lista.count(Nombre) == 1 and BaseDatos[Nombre] == Contrasenia:
+            return print(f'{Nombre} ya esta registrado, inicie sesion')
+        else:
+            AddData = {Nombre: Contrasenia}
+            BaseDatos.update(AddData)
+            Esc = open('datos.json', 'w')
+            json.dump(BaseDatos, Esc, indent=4)
+            Esc.close()
+            return print(f'{Nombre} su registro fue exitoso')
+    else:
+        Registro = {Nombre: Contrasenia}
+        Esc = open('datos.json', 'w')
+        json.dump(Registro, Esc, indent=4)
+        Esc.close()
+        return print(f'{Nombre} su registro fue exitoso')
+
+def InicioDeSesion():
+    Nombre = input('Ingrese su nombre: ')
+    Contrasenia = input('Ingrese su contraseña: ')
+    Lec = open('datos.json', 'r')
+    BaseDatos = dict(json.load(Lec))
+    Lec.close()
+    lista = list(BaseDatos.keys())
+    if lista.count(Nombre) == 1:
+        if BaseDatos[Nombre] == Contrasenia:
+            return print(f'{Nombre} bienvenido.')
+        else:
+            return print('Contraseña incorrecta')
+    else:
+        return print('Usuario no registrado')
+            
+def Datos():
+    Lec = open('datos.json', 'r')
+    BaseDatos = dict(json.load(Lec))
+    Lec.close()
+    lista = list(BaseDatos.keys())
+    print(lista)
+    
+menu = True
+
+while menu:
+    print('''
+          Opción 1: Iniciar sesión
+          Opción 2: Registrarse
+          Opción 3: Ver datos
+          Opción 4: Salir          
+          ''')
+    try:
+        Opcion = int(input('Elija una opción: '))
+        if Opcion == 1:
+            InicioDeSesion()
+        elif Opcion == 2:
+            registro()
+        elif Opcion == 3:
+            Datos()
+        elif Opcion == 4:
+            print('Hasta luego')
+            menu = False
+        else: 
+            print('Opción invalida')
+    except:
+        print('Ingrese un número')
